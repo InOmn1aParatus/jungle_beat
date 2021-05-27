6cb8aa87b6632c06eda838c0275c1606f09c5086
Matt Kragen <mattdkragen@gmail.com>
2021-05-27 11:43:17 -0700

    Add tests for nil/data/count; create #init values & #append

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,7 +1,14 @@
+require './lib/node'
+
 class LinkedList
-  attr_reader :head
+  attr_reader :head, :next_node
 
   def initialize
     @head = nil
+    @next_node = nil
+  end
+
+  def append(data)
+    @head = Node.new(data)
   end
 end
--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -10,5 +10,26 @@ RSpec.describe LinkedList do
     it 'exists' do
       expect(@list).to be_a(LinkedList)
     end
+    
+    it 'starts with nil head' do
+      expect(@list.head).to eq(nil)
+    end
+  end
+
+  describe 'methods' do
+    before :each do
+      @list = LinkedList.new
+    end
+
+    it 'appends head with data' do
+      @list.append('doop')
+      expect(@list.head.data).to eq('doop')
+      expect(@list.head.next_node).to eq(nil)
+    end
+
+    it 'counts number of items in list' do
+      @list.append('doop')
+      expect(@list.count).to eq(1)
+    end
   end
 end
